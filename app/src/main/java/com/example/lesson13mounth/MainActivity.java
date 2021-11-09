11245c3e897e7b209f88572d9fe6f9fd32eb3b08
karimovArlen <arlen.mm23@gmail.com>
2021-11-09 18:48:16 +0600

    all changes

--- app/src/main/java/com/example/lesson13mounth/MainActivity.java
+++ app/src/main/java/com/example/lesson13mounth/MainActivity.java
@@ -9,7 +9,9 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button go;
     private TextInputEditText pass_btn , user_btn;
+    private ImageView car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +27,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         user_btn = findViewById(R.id.f1);
         pass_btn = findViewById(R.id.f2);
+        car = findViewById(R.id.car_img);
         go = findViewById(R.id.GO);
+
+        Glide.with(this).load("https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg").into(car);
 
 
         user_btn.addTextChangedListener(new TextWatcher() {
@@ -54,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
+
     });
     }
 }
